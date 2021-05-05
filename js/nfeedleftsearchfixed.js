a9068430f7e10665350ac57bec8d2edbb77add36
Tanmayee Choudhury <66910404+tanmayee-07@users.noreply.github.com>
2021-05-05 19:32:52 +0530

    Add files via upload

---
+++ js/nfeedleftsearchfixed.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+   $(window).bind('scroll', function() {
+   var nHeight = 300;
+		 if ($(window).scrollTop() > nHeight) {
+			 $('.nfeedleftsearch').addClass('nfeedleftsearchfixed');
+			 $('.header').addClass('nbodyleftmargintop');
+			 $('.nfeedrightsearch').addClass('nbodyleftmarginbottom');
+		 }
+		 else {
+			 $('.nfeedleftsearch').removeClass('nfeedleftsearchfixed');
+			 $('.header').removeClass('nbodyleftmargintop');
+			 $('.nfeedrightsearch').removeClass('nbodyleftmarginbottom');
+		 }
+	});
+});
